d9307974ed9e60e5a529714ab5d8d4fc1bceeea9
Kristian Welsh <kriswelsh@gmail.com>
2020-02-28 23:00:32 +0000

    Refactoring
    Saw some room for improvement, and it seemed a fun opportunity to revise lisp

--- fizzbuzz.lisp
+++ fizzbuzz.lisp
@@ -1,16 +1,31 @@
 ;gnu clisp 2.49
 
+(defun concat (string1 string2)
+    (concatenate 'string string1 string2)
+)
+
 ; program
 
+
 (defun fizzbuzz (in)
-    (setq return "")
-    (if (eq (mod in 3) 0)
-        (setq return (concatenate 'string return "fizz")))
-    (if (eq (mod in 5) 0)
-        (setq return (concatenate 'string return "buzz")))
-    (if (or (string= return "") (eq in 0))
-        in
-        return)
+    (setq result "")
+    (if (divisible-by in 3)
+        (setq result (concat result "fizz")))
+    (if (divisible-by in 5)
+        (setq result (concat result "buzz")))
+    (if (is-empty result)
+        (setq result in))
+    (write-to-string result)
+)
+
+(defun divisible-by (arg divisor)
+    (and
+        (eq (mod arg divisor) 0)
+        (not (eq arg 0)))
+)
+
+(defun is-empty (string)
+    (string= string "")
 )
 
 ; test harness
@@ -18,46 +33,65 @@
 (setq testout "")
 (setq failures "")
 
-(defun fail-message (e a)
-    (concatenate 'string "Expected " e ", but got " a ".  |  ")
+(defun test-fizzbuzz (expected input)
+    (test (write-to-string expected) (fizzbuzz input))
 )
 
 (defun test (expected actual)
-    (let ((e (write-to-string expected)) (a (write-to-string actual)))
-        (if (string= e a)
-            (setq testout (concatenate 'string testout "."))
-            (progn
-                (setq testout (concatenate 'string testout "F"))
-                (setq failures (concatenate 'string failures (fail-message e a)))
-            )
-        )
-    )
+    (if (string= expected actual)
+        (add-success)
+        (add-failure expected actual))
 )
 
-(defun test-fizzbuzz (expected input)
-    (test expected (fizzbuzz input))
+(defun add-success ()
+    (setq testout (concat testout "."))
 )
+
+(defun add-failure (expected actual)
+    (setq testout (concat testout "F"))
+    (setq failures (concat failures (fail-message expected actual) ))
+)
+
+(defun fail-message (e a)
+    (concatenate 'string "Expected " e ", but got " a ".~%")
+)
+
+(defun print-results ()
+    (format t (concat testout "~%"))
+    (format t failures)
+)
+
 
 ; tests
 
-(test-fizzbuzz 1 1)
-(test-fizzbuzz 2 2)
-(test-fizzbuzz "fizz" 3)
-(test-fizzbuzz "buzz" 5)
-(test-fizzbuzz "fizz" 6)
-(test-fizzbuzz 7 7)
-(test-fizzbuzz "fizz" 9)
-(test-fizzbuzz "buzz" 10)
-(test-fizzbuzz "fizzbuzz" 15)
+(defun run-tests ()
+    ; standard
+    (test-fizzbuzz 1 1)
+    (test-fizzbuzz 2 2)
+    (test-fizzbuzz "fizz" 3)
+    (test-fizzbuzz "buzz" 5)
+    (test-fizzbuzz "fizz" 6)
+    (test-fizzbuzz 7 7)
+    (test-fizzbuzz "fizz" 9)
+    (test-fizzbuzz "buzz" 10)
+    (test-fizzbuzz "fizzbuzz" 15)
+    (test-fizzbuzz "fizzbuzz" 45)
+    
+    ; edge cases & complicated proofs
+    (test-fizzbuzz 0 0)
+    (test-fizzbuzz "fizz" -3)
+    (test-fizzbuzz "fizzbuzz" (large-with-factors))
+    (test-fizzbuzz (large-no-factors) (large-no-factors))
 
-(test-fizzbuzz 0 0)
-(test-fizzbuzz "fizz" (* 3 1234))
-(test-fizzbuzz "buzz" (* 5 1234))
-(test-fizzbuzz "fizzbuzz" (* 3 5 1234))
-(test-fizzbuzz (- (* 3 5 1234) 1) (- (* 3 5 1234) 1))
+    (print-results)
+)
 
-; print test results
+(defun large-with-factors ()
+    (* 3 5 9999)
+)
 
-(print testout)
-(if (not (string= failures ""))
-    (print failures))
+(defun large-no-factors ()
+    (- (large-with-factors) 1)
+)
+
+(run-tests)
